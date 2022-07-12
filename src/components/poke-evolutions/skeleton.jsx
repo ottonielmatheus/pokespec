1c1d1c8e62b3d0abce717b0fdce8e6494eb0cf8f
ottonielmatheus <ottoniel.matheus@gmail.com>
2022-07-12 02:51:19 -0300

    style: 💄 skeleton layout fix

--- src/components/poke-evolutions/skeleton.jsx
+++ src/components/poke-evolutions/skeleton.jsx
@@ -5,9 +5,9 @@ function PokeEvolutionsSkeleton() {
     <div className='skeleton' style={{ display: 'flex', flexDirection: 'column', gap: '24px' }}>
       <div style={{ width: '84px', height: '20px', backgroundColor: '#2c2c2c' }}></div>
       <div style={{ display: 'flex', gap: '72px', padding: '8px 24px' }}>
-        <div style={{ width: '90px', height: '90px', borderRadius: '50%', backgroundColor: '#2c2c2c' }}></div>
-        <div style={{ width: '90px', height: '90px', borderRadius: '50%', backgroundColor: '#2c2c2c' }}></div>
-        <div style={{ width: '90px', height: '90px', borderRadius: '50%', backgroundColor: '#2c2c2c' }}></div>
+        <div style={{ width: '60px', height: '60px', borderRadius: '50%', backgroundColor: '#2c2c2c' }}></div>
+        <div style={{ width: '60px', height: '60px', borderRadius: '50%', backgroundColor: '#2c2c2c' }}></div>
+        <div style={{ width: '60px', height: '60px', borderRadius: '50%', backgroundColor: '#2c2c2c' }}></div>
       </div>
     </div>
   )
